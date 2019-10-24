810227fc07d614d9cd1cea91164d98c2fb264972
Kevin Darcy <neoglitch822@yahoo.com>
2019-10-23 22:08:39 -0400

    added bands in town, tweaked spotify function to have switches... thanks Jake!

--- liri.js
+++ liri.js
@@ -1,55 +1,70 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
-var Spotify = require("node-spotify-api");
-//-----------------
-var spotify = new Spotify(keys.spotify);
-console.log(process.argv[2]);
-var command = process.argv[2];
-var searchField = "";
+var moment = require("moment");
 
-var i;
-for (i = 3; i < process.argv.length; i++) {
-    searchField = searchField + process.argv[i] + " ";
-    console.log(searchField);
+/* -- Spotify Vars -- */
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+var searchField = process.argv.slice(3).join(" ");
+
+/* -- Concert Vars -- */
+var concertQuery = "https://rest.bandsintown.com/artists/" + searchField + "/events?app_id=codingbootcamp"
+
+
+/* ---------------- Switches ----------------*/
+switch (command) {
+  // case "spotify-this-song":
+  //   spotThis()
+  //    break:
+  case "concert-this":
+    concertThis()
+  //   break:
 }
 
-if (command === "spotify-this-song") {
-    spotify.search({ type: 'track', query: searchField }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-        
-        console.log(data.tracks.items[0].artists[0].name);
-        console.log(data.tracks.items[0].artists[0].external_urls.spotify);
-        console.log(data.tracks.items[0].album.name);
-        console.log(data.tracks.items[1].name);
-      });
-    }
+/* ---------------- Spotify This ------------------*/
+function spotThis() {
+  if (command === "spotify-this-song") {
+    spotify.search({
+      type: 'track',
+      query: searchField
+    }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      console.log("----------BAND NAME")
+      console.log(data.tracks.items[0].artists[0].name);
+      console.log("\n----------SPOTIFY LINK")
+      console.log(data.tracks.items[0].artists[0].external_urls.spotify);
+      console.log("\n----------BAND NAME")
+      console.log(data.tracks.items[0].album.name);
+      console.log("\n----------ALBUM")
+      console.log(data.tracks.items[1].name);
+      console.log("-------------------------------------")
+    });
+  }
+}
 
-// Concert This
-// node liri.js concert-this <artist/band name here>
-
-// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
-
-// // Spotify this song
-// // node liri.js spotify-this-song '<song name here>'
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
-//     }
-   
-//   console.log(data.tracks.items[0].artists[0].name);
-//   console.log(data.tracks.items[0].artists[0].external_urls.spotify);
-//   console.log(data.tracks.items[0].album.name);
-//   console.log(data.tracks.items[1].name);
-//   });
+/* ---------------- Concert This ------------------*/
+function concertThis() {
+axios.get(concertQuery).then(function (response) { 
+  console.log("Venue Name: " + response.data[0].venue.name);
+  console.log("------------------");
+  console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
+  console.log("------------------");
+  console.log("Event Date: " + (response.data[0].datetime).format("MM/DD/YYYY"));
+  console.log("------------------");
+  
+}).catch(function (error) {
+	
+	console.log(error)
+})
+};
 
 
+/* ---------------- Movie This ------------------*/
 
 
 
-// Movie This
-
-// Do what it says
+/* ---------------- Do What It Says ------------------*/
